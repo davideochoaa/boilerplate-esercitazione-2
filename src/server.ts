4d25dd1ba9f189af44c2860b14c8087089afe08a
Davide Ochoa <davideochoaa@gmail.com>
2022-05-20 17:50:37 +0200

    feat: modifiche

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import  express from "express";
 import router from "./router";
 import bodyParser from "body-parser";
-
+//npm run dev
 const server = express();
 //server.use(express.json());
 server.use(bodyParser.json())
